3a3e533231829015072bdb6852bdb8ccd6f17c17
Axel Rindle <mail@axelrindle.de>
2021-02-21 12:58:10 +0100

    Fix

--- index.d.ts
+++ index.d.ts
@@ -1,17 +1,17 @@
-export namespace wog {
-
-	export * from './lib/accounts'
-	export * from './lib/database'
-	export * from './lib/logger'
-	export * from './lib/mailer'
-	export * from './lib/packages'
-	export * from './lib/redis'
-	export * from './lib/storage'
-
-}
-
 declare global {
 
-	export * from './lib/globals'
+	/** Holds the application root directory. */
+	const ROOT_DIRECTORY: string
+
+	/** Indicates whether the application is running in debug mode. */
+	const IS_DEBUG: boolean
 
 }
+
+export * from './lib/accounts'
+export * from './lib/database'
+export * from './lib/logger'
+export * from './lib/mailer'
+export * from './lib/packages'
+export * from './lib/redis'
+export * from './lib/storage'
--- lib/globals.d.ts
+++
@@ -1,5 +0,0 @@
-/** Holds the application root directory. */
-export const ROOT_DIRECTORY: string
-
-/** Indicates whether the application is running in debug mode. */
-export const IS_DEBUG: boolean
