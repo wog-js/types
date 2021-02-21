19d2052c5ec8f6280a947b008b14a46f375c29be
Axel Rindle <mail@axelrindle.de>
2021-02-21 12:31:38 +0100

    Collect exports into a namespace

--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,11 @@
-export * from './lib/accounts'
-export * from './lib/database'
-export * from './lib/logger'
-export * from './lib/mailer'
-export * from './lib/packages'
-export * from './lib/redis'
-export * from './lib/storage'
+export namespace wog {
+
+	export * from './lib/accounts'
+	export * from './lib/database'
+	export * from './lib/logger'
+	export * from './lib/mailer'
+	export * from './lib/packages'
+	export * from './lib/redis'
+	export * from './lib/storage'
+
+}
